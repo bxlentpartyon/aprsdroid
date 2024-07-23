efcbdf196e2b8b225bb3cda5cac372168717ab01
Alex Thorlton <gathorlton@gmail.com>
2024-07-22 21:24:22 -0500

    Switch to MultiDexApplication
    This is needed to fix the following build error:
    
    <snip>
    Cannot fit requested classes in a single dex file (# methods: 81090 > 65536)
    </snip>
    
    See the following for more info:
    
    https://developer.android.com/build/multidex
    
    This ultimately brings the time for a development/debug build down
    from about 3m30s down to 30s or less.

--- src/APRSdroidApplication.scala
+++ src/APRSdroidApplication.scala
@@ -1,8 +1,9 @@
 package org.aprsdroid.app
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 
-class APRSdroidApplication extends Application {
+class APRSdroidApplication extends MultiDexApplication {
 
 	override def onCreate() {
 		super.onCreate()
